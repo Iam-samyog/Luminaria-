30ff2ba1f58f9b44ed4c4cc4811ab723b0a954b2
Iam-samyog <msamyog37@gmail.com>
2025-06-08 21:09:58 +0545

    Class Based Post View

--- Luminaria/blog/views.py
+++ Luminaria/blog/views.py
@@ -1,6 +1,8 @@
 from django.core.paginator import EmptyPage,Paginator,PageNotAnInteger
 from django.shortcuts import render,get_object_or_404
 from .models import Post
+from django.views.generic import ListView
+
 # Create your views here.
 def post_list(request):
     post_list=Post.published.all()
@@ -36,3 +38,14 @@ def post_detail(request,year,month,day,post):
         'blog/post/detail.html',
         {'post':post}
     )
+
+class PostListView(ListView):
+    """
+    Alternative post list view
+    
+    
+    """
+    queryset=Post.published.all()
+    context_object_name='posts'
+    paginate_by=3
+    template_name='blog/post/list.html'
