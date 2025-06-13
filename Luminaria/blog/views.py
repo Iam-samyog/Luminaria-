ead3bcb8036fe01a4484a6a3854db997f93d3c56
Iam-samyog <msamyog37@gmail.com>
2025-06-13 22:13:10 +0545

    Added the functionality to recomment similar posts

--- Luminaria/blog/views.py
+++ Luminaria/blog/views.py
@@ -6,6 +6,7 @@ from .forms import EmailPostForm,CommentForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
+from django.db.models import Count
 # Create your views here.
 def post_list(request,tag_slug=None):
     post_list=Post.published.all()
@@ -47,6 +48,16 @@ def post_detail(request,year,month,day,post):
     #Form for users to comment
     form=CommentForm()
     
+    #list of similar post
+    post_tags_ids=post.tags.values_list('id',flat=True)
+    similar_posts=Post.published.filter(
+        tags__in=post_tags_ids
+    ).exclude(id=post.id)
+    similar_posts=similar_posts.annotate(
+        same_tags=Count('tags')
+        
+    ).order_by('-same_tags','-publish')[:4]
+    
     return render(
         request,
         'blog/post/detail.html',
@@ -54,7 +65,7 @@ def post_detail(request,year,month,day,post):
             'post':post,
             'comments':comments,
             'form':form,
-            
+            'similar_posts':similar_posts
          
          }
     )
