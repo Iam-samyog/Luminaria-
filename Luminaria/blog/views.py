f5aee9e7580bb084ec84f64d09ea258ef0260e02
Iam-samyog <msamyog37@gmail.com>
2025-06-14 15:05:02 +0545

    Build the basic search engine for the blog

--- Luminaria/blog/views.py
+++ Luminaria/blog/views.py
@@ -2,11 +2,13 @@ from django.core.paginator import EmptyPage,Paginator,PageNotAnInteger
 from django.shortcuts import render,get_object_or_404
 from .models import Post
 from django.views.generic import ListView
-from .forms import EmailPostForm,CommentForm
+from .forms import EmailPostForm,CommentForm,SearchForm
 from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
+from django.contrib.postgres.search import SearchVector
+
 # Create your views here.
 def post_list(request,tag_slug=None):
     post_list=Post.published.all()
@@ -150,5 +152,31 @@ def post_comment(request,post_id):
             'post':post,
             'form':form,
             'comment':comment
+        }
+    )
+
+#search view
+def post_search(request):
+    form=SearchForm
+    query=None
+    results=[]
+    
+    if 'query' in request.GET:
+        form=SearchForm(request.GET)
+        if form.is_valid():
+            query=form.cleaned_data['query']
+            results=(
+                Post.published.annotate(
+                    search=SearchVector('title','body')
+                ).filter(search=query)
+            )
+    
+    return render(
+        request,
+        'blog/post/search.html',
+        {
+            'form':form,
+            'query':query,
+            'results':results
         }
     )
