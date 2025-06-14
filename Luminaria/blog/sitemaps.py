1f1157830b79d6a7510952fb558ce007326ef813
Iam-samyog <msamyog37@gmail.com>
2025-06-14 17:13:55 +0545

    Add TagSitemap to include tag pages in sitemap

--- Luminaria/blog/sitemaps.py
+++ Luminaria/blog/sitemaps.py
@@ -1,14 +1,27 @@
 from django.contrib.sitemaps import Sitemap
 from .models import Post
+from taggit.models import Tag
+from django.urls import reverse
 
 class PostSitemap(Sitemap):
-    changefreq='weekly'
-    priority=0.9
-    
+    changefreq = 'weekly'
+    priority = 0.9
+
     def items(self):
         return Post.published.all()
-    
-    def lastmod(self,obj):
+
+    def lastmod(self, obj):
         return obj.updated
-    
+
+
+class TagSitemap(Sitemap):
+    changefreq = 'weekly'
+    priority = 0.7
+
+    def items(self):
+        return Tag.objects.all()
+
+    def location(self, obj):
+        return reverse('blog:post_list_by_tag', kwargs={'tag_slug': obj.slug})
+
     
