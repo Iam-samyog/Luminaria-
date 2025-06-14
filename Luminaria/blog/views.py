10adc99bf9157c15564d6451f825010c423af162
Iam-samyog <msamyog37@gmail.com>
2025-06-14 15:19:51 +0545

    Made the search engine with trigrams functionality

--- Luminaria/blog/views.py
+++ Luminaria/blog/views.py
@@ -12,6 +12,7 @@ from django.contrib.postgres.search import (
  SearchQuery,
  SearchRank
 )
+from django.contrib.postgres.search import TrigramSimilarity
 
 # Create your views here.
 def post_list(request,tag_slug=None):
@@ -169,13 +170,12 @@ def post_search(request):
         form=SearchForm(request.GET)
         if form.is_valid():
             query=form.cleaned_data['query']
-            search_vector=SearchVector('title',weight='A')+SearchVector('body',weight='B')
-            search_query=SearchQuery(query,config='spanish')
+          
             results=(
                 Post.published.annotate(
-                    search=search_vector,
-                    rank=SearchRank(search_vector,search_query)
-                ).filter(rank__gte=0.3).order_by('-rank')
+                    similarity=TrigramSimilarity('title', query),
+
+                ).filter(similarity__gt=0.1).order_by('-similarity')
             )
     
     return render(
