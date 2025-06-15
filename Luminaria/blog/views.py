bc02a852211cc598ef6d07ddd61d053a505caf93
Iam-samyog <msamyog37@gmail.com>
2025-06-15 18:50:47 +0545

    Added the Bootstrap and design of the code

--- Luminaria/blog/views.py
+++ Luminaria/blog/views.py
@@ -16,9 +16,6 @@ from django.contrib.postgres.search import TrigramSimilarity
 
 # Create your views here.
 
-def home(request):
-    
-    return render(request, 'blog/home.html')
 
 
 def post_list(request,tag_slug=None):
