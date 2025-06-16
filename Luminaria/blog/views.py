dcac6b1ea3683b3257f88e63f13fbe14c3f75240
Iam-samyog <msamyog37@gmail.com>
2025-06-16 10:11:54 +0545

    Removed uneccsaaary contnent

--- Luminaria/blog/views.py
+++ Luminaria/blog/views.py
@@ -7,11 +7,7 @@ from django.core.mail import send_mail
 from django.views.decorators.http import require_POST
 from taggit.models import Tag
 from django.db.models import Count
-from django.contrib.postgres.search import (
- SearchVector,
- SearchQuery,
- SearchRank
-)
+
 from django.contrib.postgres.search import TrigramSimilarity
 
 # Create your views here.
